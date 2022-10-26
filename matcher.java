0eae7f2e4c5e3c0a834b7133a68137265f0c6a9f
Eli Hamaker <59748007+possibl@users.noreply.github.com>
2022-10-25 21:49:43 -0500

    Update matcher.java
    4 basic groups

--- matcher.java
+++ matcher.java
@@ -15,54 +15,89 @@ public class matcher{
          * 
          *  
          */
-        //lunch#'s to binary so things can be kept track of. This should be moved to another file.
-        //Original lunch #'s
-        String[][] peopleIntel = 
+        //all info. Should be in a seperate text file. This is for testing
+        int[][] peopleIntel = 
         {
-            {"12345"},
-            {"23456"},
-            {"34567"},
-            {"45678"},
-            {"56789"},
-            {"99999"}
+            {12345,0,1},
+            {23456,0,0},
+            {34567,0,0},
+            {45678,1,1},
+            {56789,1,1},
+            {99999,1,0}
         };
 
-        String[] test = peopleIntel[][0];
+        //id info
+        int[] id = new int[peopleIntel.length];
+        for (int i = 0; i<id.length; i++){
+            id[i] = peopleIntel[i][0];
+            System.out.println(id[i]);
+        }
 
-        //accepts int[]. defaults to 32 digits of binary
-        num2Binary lunchNum = new num2Binary("%17s");
-        peopleIntel = lunchNum.convert2Binary(peopleIntel[1]);
-
-/*
- * String[] peopleinfo = 
-        {
-            "12345",
-            "23456",
-            "34567",
-            "45678",
-            "56789",
-            "99999"
-        };
-
-        //accepts int[]. defaults to 32 digits of binary
-        num2Binary lunchNum = new num2Binary("%17s");
-        String[] lunchNumBinary = lunchNum.convert2Binary(lunchNumAscii);
- */
-
-        //ammend all info about peeps to this
-        /*
-        int[] people = 
-        {
-            0b01,
-            0b10,
-            0b00,
-            0b00,
-            0b11,
-            0b11
-        };
-        
-        //sortedGP(people);
+        /* 
+         * Sorting Straight Males, Straight Females, Gay Males, Lesbian Females, etc 
+         * mfGroup, fmGroup, mmGroup, ffGroup, etc
+         * gender-PerfeeredPartnersGender
+         * 
+         * 
+         * 
+         * THIS MUST BE MADE INTO A CLASS AND 
+         * Surrently there is an  logic error that means people are not being assigned a group
+         * 
+         * 
         */
+
+        //mfGroup
+        int[][] mmGroup = new int[peopleIntel.length][peopleIntel[0].length];
+        int[][] mfGroup = new int[peopleIntel.length][peopleIntel[0].length];
+        int[][] ffGroup = new int[peopleIntel.length][peopleIntel[0].length];
+        int[][] fmGroup = new int[peopleIntel.length][peopleIntel[0].length];
+        for (int i = 0; i<peopleIntel.length; i++){
+            //mmGroup
+            if (peopleIntel[i][2] == 0) {
+                if (peopleIntel[i][1] == 0){
+                    mmGroup[i][2] = peopleIntel[i][2];
+                    mmGroup[i][1] = peopleIntel[i][1];
+                    mmGroup[i][0] = peopleIntel[i][0];
+                    System.out.println("mmGroup");
+                }
+            } 
+            //mfgroup
+            if (peopleIntel[i][1] == 0) { //self sexuality identification
+                if (peopleIntel[i][2] == 1){ //preferred partner sexuality identification
+                    mfGroup[i][2] = peopleIntel[i][2];
+                    mfGroup[i][1] = peopleIntel[i][1];
+                    mfGroup[i][0] = peopleIntel[i][0];
+                    System.out.println("mfGroup");
+                }
+            } 
+            //ffgroup
+            if (peopleIntel[i][1] == 1) { //self sexuality identification
+                if (peopleIntel[i][2] == 1){ //preferred partner sexuality identification
+                    ffGroup[i][2] = peopleIntel[i][2];
+                    ffGroup[i][1] = peopleIntel[i][1];
+                    ffGroup[i][0] = peopleIntel[i][0];
+                    System.out.println("ffGroup");
+                }
+            } 
+            //fmgroup
+            if (peopleIntel[i][1] == 1) { //self sexuality identification
+                if (peopleIntel[i][2] == 0){ //preferred partner sexuality identification
+                    fmGroup[i][2] = peopleIntel[i][2];
+                    fmGroup[i][1] = peopleIntel[i][1];
+                    fmGroup[i][0] = peopleIntel[i][0];
+                    System.out.println("fmGroup");
+                }
+            } 
+            System.out.println("iterated " + (i+1) + " times");
+        }
+        //Code to verify output makes sense
+        for (int i = 0; i<peopleIntel.length; i++){
+            System.out.println(mmGroup[i][0]+" mmGroup");
+            System.out.println(mfGroup[i][0]+" mfGroup");
+            System.out.println(ffGroup[i][0]+" ffGroup");
+            System.out.println(fmGroup[i][0]+" fmGroup");
+        }
+
     }
 
 
