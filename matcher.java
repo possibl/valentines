2044910b635914f255260523e6cf2bc162723e76
Eli Hamaker <59748007+possibl@users.noreply.github.com>
2022-10-27 14:20:28 -0500

    Update matcher.java

--- matcher.java
+++ matcher.java
@@ -13,17 +13,22 @@ public class matcher{
          * Organized like: Gender and Prefrence (must be compatible), Strong Hobbies (Should have 2+ aligning Strong hobbies), Strong Prefrence Hobbies (Should have 2+ aligning Strong hobbies), Weak Hobbies (ex: fav subject, lower time commitment things, recent hobbies)
          * Lunch#, Gender (0=male, 1=female), Prefrence (0=male, 1=female), movies, 
          * 
-         *  
+         *  person's grade
+         *  grade prefrence
+         * 0001 =12th
+         * 0010 = 11th
+         * 0100 = 10th
+         * 1000 = 9th
          */
         //all info. Should be in a seperate text file. This is for testing
         int[][] peopleIntel = 
         {
-            {12345,0,1},
-            {23456,0,0},
-            {34567,0,0},
-            {45678,1,1},
-            {56789,1,1},
-            {99999,1,0}
+            {12345,00000,0,1,0100,1100},
+            {23456,00000,0,1,1000,1000},
+            {34567,00000,0,1,0100,0111},
+            {45678,00000,1,0,1000,0111},
+            {56789,00000,1,0,1000,0111},
+            {99999,00000,1,0,0100,0111}
         };
 
         //id info
@@ -46,7 +51,6 @@ public class matcher{
          * 
         */
 
-        //mfGroup
         int[][] mmGroup = new int[peopleIntel.length][peopleIntel[0].length];
         int[][] mfGroup = new int[peopleIntel.length][peopleIntel[0].length];
         int[][] ffGroup = new int[peopleIntel.length][peopleIntel[0].length];
@@ -54,37 +58,37 @@ public class matcher{
         for (int i = 0; i<peopleIntel.length; i++){
             //mmGroup
             if (peopleIntel[i][2] == 0) {
-                if (peopleIntel[i][1] == 0){
-                    mmGroup[i][2] = peopleIntel[i][2];
-                    mmGroup[i][1] = peopleIntel[i][1];
-                    mmGroup[i][0] = peopleIntel[i][0];
+                if (peopleIntel[i][3] == 0){
+                    for (int k = 0; k < mmGroup[i].length; k++){
+                        mmGroup[i][k] = peopleIntel[i][k];
+                    }
                     System.out.println("mmGroup");
                 }
             } 
             //mfgroup
-            if (peopleIntel[i][1] == 0) { //self sexuality identification
-                if (peopleIntel[i][2] == 1){ //preferred partner sexuality identification
-                    mfGroup[i][2] = peopleIntel[i][2];
-                    mfGroup[i][1] = peopleIntel[i][1];
-                    mfGroup[i][0] = peopleIntel[i][0];
+            if (peopleIntel[i][2] == 0) { //self sexuality identification
+                if (peopleIntel[i][3] == 1){ //preferred partner sexuality identification
+                    for (int k = 0; k < mmGroup[i].length; k++){
+                        mfGroup[i][k] = peopleIntel[i][k];
+                    }
                     System.out.println("mfGroup");
                 }
             } 
             //ffgroup
-            if (peopleIntel[i][1] == 1) { //self sexuality identification
-                if (peopleIntel[i][2] == 1){ //preferred partner sexuality identification
-                    ffGroup[i][2] = peopleIntel[i][2];
-                    ffGroup[i][1] = peopleIntel[i][1];
-                    ffGroup[i][0] = peopleIntel[i][0];
+            if (peopleIntel[i][2] == 1) { //self sexuality identification
+                if (peopleIntel[i][3] == 1){ //preferred partner sexuality identification
+                    for (int k = 0; k < mmGroup[i].length; k++){
+                        ffGroup[i][k] = peopleIntel[i][k];
+                    }
                     System.out.println("ffGroup");
                 }
             } 
             //fmgroup
-            if (peopleIntel[i][1] == 1) { //self sexuality identification
-                if (peopleIntel[i][2] == 0){ //preferred partner sexuality identification
-                    fmGroup[i][2] = peopleIntel[i][2];
-                    fmGroup[i][1] = peopleIntel[i][1];
-                    fmGroup[i][0] = peopleIntel[i][0];
+            if (peopleIntel[i][2] == 1) { //self sexuality identification
+                if (peopleIntel[i][3] == 0){ //preferred partner sexuality identification
+                    for (int k = 0; k < mmGroup[i].length; k++){
+                        fmGroup[i][k] = peopleIntel[i][k];
+                    }
                     System.out.println("fmGroup");
                 }
             } 
@@ -92,10 +96,29 @@ public class matcher{
         }
         //Code to verify output makes sense
         for (int i = 0; i<peopleIntel.length; i++){
+
+
             System.out.println(mmGroup[i][0]+" mmGroup");
             System.out.println(mfGroup[i][0]+" mfGroup");
             System.out.println(ffGroup[i][0]+" ffGroup");
             System.out.println(fmGroup[i][0]+" fmGroup");
+        }
+
+        //Splitting people based off age prefrence
+        //mf and fm for now
+        for (int i = 0; i<mmGroup.length; i++){
+            for (int k = 0; k<mfGroup.length; k++){
+                mmGroup[i][1] = mfGroup[k][0];
+                mfGroup[k][1] = mmGroup[i][0];
+            }
+        }
+
+        for (int i = 0; i<mmGroup.length; i++){
+            System.out.println(mmGroup[i][0]);
+            System.out.println(mmGroup[i][1]);
+
+            System.out.println(mfGroup[i][0]);
+            System.out.println(mfGroup[i][1]);
         }
 
     }
